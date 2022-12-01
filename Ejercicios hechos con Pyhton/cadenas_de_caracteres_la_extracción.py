7d0c7022e5cb4e19dcbc3a1354920d15178c3bb8
Jhonattan Florez <Jdflorez038@misena.edu.co>
2022-12-01 13:14:51 +0100

    Aporte para el informe

--- Ejercicios hechos con Pyhton/cadenas_de_caracteres_la_extracción.py
+++ Ejercicios hechos con Pyhton/cadenas_de_caracteres_la_extracción.py
@@ -1,7 +1,11 @@
-print ("Estaremos usando la extracción")
-print ("Hola mundo,estamos usando la extracción")
+print ("Hola mundo, estamos usando la extracción")
+# Creamos una cadena de texto
 mensaje = "Hola mundo,estamos  usando la extracción"
+# Extraemos una subcadena de la cadena de texto determinando desde que caracter hasta que caracter
 extraer_subcadena = mensaje[11:28]
-print (extraer_subcadena)
-
-
+# Extraemos solamente el "hola" de la cadena de texto 
+extraer_hola= mensaje[0:4]
+# Hola de la cadena de texto
+print ("Hemos extraido: ",extraer_hola)
+# FRagmento de la cadena de texto
+print ("Hemos extraido: ", extraer_subcadena)
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Inicios en Python
+<img align="center" src="https://github.com/devicons/devicon/blob/master/icons/python/python-original.svg" alt="Python" width="60" height="60"/>
 
 ##### In this repository you can see some exercises he realizados,  con el proposito de aprender Python.
 
-<img align="center" src="https://github.com/devicons/devicon/blob/master/icons/python/python-original.svg" title="Python" alt="Python" width="80" height="80"/>&nbsp;
+
